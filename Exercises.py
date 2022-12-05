5231e3bc4b11dd5b65c1847edd18856f45544111
ÇETİN <cetinnnarasss@gmail.com>
2022-12-05 14:07:08 +0300

    doc.

--- Exercises.py
+++ Exercises.py
@@ -1,8 +1,18 @@
-def function(): # Don't be confused, we use 'def()' to create a function. 
-                # You will see it in the next lessons.
-    """
-Hi, I am the docstring of this code. 
-If you need any information about this function or module, read me. 
-It can help you understand how the module or function works.
-    """
-print(function.__doc__)
+# def function(): # Don't be confused, we use 'def()' to create a function. 
+#                 # You will see it in the next lessons.
+#     """
+# Hi, I am the docstring of this code. 
+# If you need any information about this function or module, read me. 
+# It can help you understand how the module or function works.
+#     """
+# print(function.__doc__)
+
+# print(print.__doc__)
+# print(value, ..., sep=' ', end='\n', file=sys.stdout, flush=False)
+
+# Prints the values to a stream, or to sys.stdout by default.
+# Optional keyword arguments:
+# file:  a file-like object (stream); defaults to the current sys.stdout.
+# sep:   string inserted between values, default a space.
+# end:   string appended after the last value, default a newline.
+# flush: whether to forcibly flush the stream.
