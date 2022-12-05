404d62669536cd56f3764d21654c235badb8cd31
ÇETİN <cetinnnarasss@gmail.com>
2022-12-05 14:04:44 +0300

    first commit

---
+++ Exercises.py
@@ -0,0 +1,8 @@
+def function(): # Don't be confused, we use 'def()' to create a function. 
+                # You will see it in the next lessons.
+    """
+Hi, I am the docstring of this code. 
+If you need any information about this function or module, read me. 
+It can help you understand how the module or function works.
+    """
+print(function.__doc__)
